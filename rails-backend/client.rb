e2b1c3835fbbc542e366b66d0ddc47fe4df26839
Jp1200 <jprkr96@gmail.com>
2020-04-13 09:49:20 -0500

    css added

--- rails-backend/client.rb
+++ rails-backend/client.rb
@@ -3,3 +3,28 @@ CLIENT_ID = 04OIN2AY10DSIZQ5SAH2VUDWMGBDENIVGBGAHDBZKFCWLWOR
 CLIENT_SECRET =1MU4G2V2SQ3VRQNVAML343FUSBJEDKYLQJ24RTKJH022V5ZB
 # API for FourSquare
 # for more info: https://developer.foursquare.com/docs/api-reference/venues/search/
+
+
+# https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=04OIN2AY10DSIZQ5SAH2VUDWMGBDENIVGBGAHDBZKFCWLWOR&client_secret=1MU4G2V2SQ3VRQNVAML343FUSBJEDKYLQJ24RTKJH022V5ZB&v=20200412
+# 'High of playing God, Larry&theBears, MOxxy, Conundrum' -> var= ''
+# var.subg(' ',s/\'')
+# bandsarray = [highCostofPlatinngod,
+# Larry
+# MOxxy
+# condunrim]
+#
+# bands playing at this show: _____ ______ _____ ____ ->
+# bandcamp/${bandsarray[0]} href ='https://bandcamp.com/highcostofplayinggod' -> catcherror ->404 page error
+# alert ('no bandcamp') -> error 'Bandcamp not found! sorry ):'
+# fetch('http://localhost:3000/events/bands')
+# .then(response=> response.json())
+# .then(json=>{
+#   json=> DoSTUFF
+#   })
+#   fethc(user )
+#   .then (creds are right)
+#   .then login in again
+# usernmae:ffdfdf
+# password: dfdfjdf
+# edit: hengres they luke
+# hometown: austin tx
